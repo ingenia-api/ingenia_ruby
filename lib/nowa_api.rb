4f9008d8d9825175a41142f9abe8e863d941c111
Ivan Kocienski <ivan.kocienski@gmail.com>
2013-09-25 10:07:17 +0100

    now verify api key. again. very safe

--- lib/nowa_api.rb
+++ lib/nowa_api.rb
@@ -10,8 +10,6 @@ require 'nowa_api/tag_set'
 module Nowa
   module Api
 
-    attr_accessor :api_key
-
     extend self
     
     class CallFailed < StandardError
@@ -24,17 +22,17 @@ module Nowa
 
     def classify(text)
       debug { "classify" }
-      verify_response { Remote.post('/classify', :api_key => @api_key, :text => text) }
+      verify_response { Remote.post('/classify', :api_key => api_key, :text => text) }
     end
 
     def train(text, tags = {})
       debug { "train" }
       if tags.is_a? Array
 
-        verify_response { Remote.post('/items', :api_key => @api_key, :json => { :text => text, :tags => tags }.to_json) }
+        verify_response { Remote.post('/items', :api_key => api_key, :json => { :text => text, :tags => tags }.to_json) }
 
       elsif tags.is_a? Hash
-        verify_response { Remote.post('/items', :api_key => @api_key, :json => { :text => text, :tag_sets => tags }.to_json) }
+        verify_response { Remote.post('/items', :api_key => api_key, :json => { :text => text, :tag_sets => tags }.to_json) }
 
       else
         raise "Nowa::Api.train(text, tags) must be called with tags argument as either an Array or a Hash"
@@ -43,17 +41,27 @@ module Nowa
 
     def similar_to(item_id, limit = 10)
       debug { "similar_to" }
-      verify_response { Remote.get("/similar_to/#{item_id}", :api_key => @api_key, :limit => limit ) }
+      verify_response { Remote.get("/similar_to/#{item_id}", :api_key => api_key, :limit => limit ) }
     end
 
     def trained_tags
       debug { "trained_tags" }
-      verify_response { Remote.get('/learnt_tags', :api_key => @api_key) }
+      verify_response { Remote.get('/learnt_tags', :api_key => api_key) }
     end
     
     def endpoint=(ep)
       debug { "endpoint=#{Remote.endpoint}" }
       Remote.endpoint = ep
+    end
+
+    def api_key=(k)
+      debug { "api_key=#{k}" }
+      @api_key = k
+    end
+
+    def api_key
+      raise 'Nowa::Api.api_key not set' if @api_key.nil?
+      @api_key
     end
 
     def debug=(dbg)
