7a1bc21cbe6d02010ad2d917cc3768d187fa88a8
Joran Kikke <joran.k@gmail.com>
2014-02-25 10:23:31 +0000

    fixed to classifier example

--- examples/classify.rb
+++ examples/classify.rb
@@ -6,12 +6,4 @@ require 'nowa_api'
 
 Nowa::Api.api_key = "API_KEY"
 
-# text = <<BIG
-# we ate at 7pm straight from a viewing at the cinema. Nice table, nice food, decent service and best of all, good value. Would I go again? Already booked...
-# BIG
-
-puts Nowa::Item.all 
-
-
-
-# puts RestClient.post('https://api.ingeniapi.com/similar_to_text', :text => text, :api_key => Nowa::Api.api_key)
+puts Nowa::Api.classify "What kind of cheese is the best cheese?"
