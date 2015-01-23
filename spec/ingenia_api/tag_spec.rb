97e3fcadd0676c8932ab49bdc0019791cf67dc89
Philip Stevens <phil@retechnica.com>
2015-01-23 16:15:31 +0000

    Create a merge function and implement spec.

--- spec/ingenia_api/tag_spec.rb
+++ spec/ingenia_api/tag_spec.rb
@@ -1,76 +1,70 @@
 require 'spec_helper'
 
 describe Ingenia::Tag do
-  let( :empty_api_response ) { { 'status' => 'okay', 'data' => {} } }
+  let(:empty_api_response) { { 'status' => 'okay', 'data' => {} } }
 
 
   describe '#create' do
-    it 'calls post' do
-      name = "this is a test tag"
+    let(:name) { 'this is a test tag' }
+    let(:path) { '/tags' }
+    let(:request) { { json: "{\"name\":\"#{name}\",\"tag_set_id\":1}", api_key: "1234" } }
 
-      expected_path = '/tags'
-      expected_request = {:json=>"{\"name\":\"#{name}\",\"tag_set_id\":1}", :api_key=>"1234"}
-
-      expect(Ingenia::Api::Remote).to receive( :post ).
-        with( expected_path, expected_request).
-        and_return( empty_api_response )
-
-      Ingenia::Tag.create(:name => name, :tag_set_id => 1)
+    specify do
+      expect(Ingenia::Api::Remote).to receive(:post).with(path, request) { empty_api_response }
+      described_class.create(name: name, tag_set_id: 1)
     end
   end
 
 
   describe '#update' do
-    it 'calls put' do
-      name = "this is an updated test tag"
+    let(:name) { 'this is an updated test tag' }
+    let(:path) { '/tags/1' }
+    let(:request) { { json: "{\"name\":\"#{name}\",\"tag_set_id\":1}", api_key: "1234" } }
 
-      expected_path = '/tags/1'
-      expected_request = {:json=>"{\"name\":\"#{name}\",\"tag_set_id\":1}", :api_key=>"1234"}
+    specify do
+      expect(Ingenia::Api::Remote).to receive(:put).with(path, request) { empty_api_response }
+      described_class.update(1, name: name, tag_set_id: 1)
+    end
+   end
 
-      expect(Ingenia::Api::Remote).to receive( :put ).
-        with( expected_path, expected_request).
-        and_return( empty_api_response )
+  describe '#merge' do
+    let(:tag_ids) { [1, 2, 3] }
+    let(:path) { '/tags/1/merge' }
+    let(:request) { { json: "{\"tag_ids\":[1,2,3]}", api_key: "1234" } }
 
-      Ingenia::Tag.update(1, :name => name, :tag_set_id => 1)
+    specify do
+      expect(Ingenia::Api::Remote).to receive(:post).with(path, request) { empty_api_response }
+      described_class.merge(1, tag_ids: tag_ids)
     end
   end
 
   describe '#get' do
-    it 'calls get' do
-      expected_path = '/tags/1'
-      expected_request = { :api_key=>"1234" }
+    let(:path) { '/tags/1' }
+    let(:request) { { api_key: "1234" } }
 
-      expect(Ingenia::Api::Remote).to receive( :get ).
-        with( expected_path, expected_request).
-        and_return( empty_api_response )
-
-      Ingenia::Tag.get(1)
+    specify do
+      expect(Ingenia::Api::Remote).to receive(:get).with(path, request) { empty_api_response }
+      described_class.get(1)
     end
   end
 
   describe '#all' do
+    let(:path) { '/tags' }
+    let(:request) { { :offset => 0, :limit => 50, :api_key => "1234" } }
+
     it 'calls get' do
-      expected_path = '/tags'
-      expected_request = { :offset => 0, :limit => 50, :api_key=>"1234" }
-
-      expect(Ingenia::Api::Remote).to receive( :get ).
-        with( expected_path, expected_request).
-        and_return( empty_api_response )
-
-      Ingenia::Tag.all :offset => 0, :limit => 50
+      expect(Ingenia::Api::Remote).to receive(:get).with(path, request) { empty_api_response }
+      described_class.all(offset: 0, limit: 50)
     end
   end
 
   describe '#delete' do
+    let(:path) { '/tags/1' }
+    let(:request) { { :params => { :api_key => "1234" } } }
+
     it 'calls delete' do
-      expected_path = '/tags/1'
-      expected_request = {:params=>{:api_key=>"1234"}}
-
-      expect(Ingenia::Api::Remote).to receive( :delete ).
-        with( expected_path, expected_request).
-        and_return( empty_api_response )
-
-      Ingenia::Tag.destroy(1)
+      expect(Ingenia::Api::Remote).to receive(:delete).with(path, request) { empty_api_response }
+      described_class.destroy(1)
     end
   end
 end
